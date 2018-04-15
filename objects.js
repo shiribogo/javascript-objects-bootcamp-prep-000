03cb7e21c9fefbbad38420a7e2fb399168fb0399
Shiri Mund <shiribogo@gmail.com>
2018-04-15 19:17:38 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {idan: 'boee'}
+function upadtePlaylist(playlist, artistName, songTitle){
+  playlist [artistName] = songTitle
+  return object
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist
+}
